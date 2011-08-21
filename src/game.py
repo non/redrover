5e88535aa5287c1829839c54402346c523342a34
Erik Osheim <d_m@plastic-idolatry.com>
2011-08-21 10:41:47 -0400

    driving around smoothly, rendering rocks, etc

--- src/game.py
+++ src/game.py
@@ -1,19 +1,34 @@
 import math, os, random, sys
 import pygame, pygame.gfxdraw
 
+try:
+    import psyco
+    psyco.full()
+except:
+    pass
+
 SCALE = 4
 
-NUM_SCREENS_WIDE = 5
-NUM_SCREENS_TALL = 5
+#NUM_SCREENS_WIDE = 5
+#NUM_SCREENS_TALL = 5
+NUM_SCREENS_WIDE = 3
+NUM_SCREENS_TALL = 3
 
 PIXEL_WIDTH = 1000
 PIXEL_HEIGHT = 700
 
-TOTAL_WIDTH = PIXEL_WIDTH * (NUM_SCREENS_WIDE - 1)
-TOTAL_HEIGHT = PIXEL_HEIGHT * (NUM_SCREENS_TALL - 1)
+#TOTAL_WIDTH = PIXEL_WIDTH * (NUM_SCREENS_WIDE - 1)
+#TOTAL_HEIGHT = PIXEL_HEIGHT * (NUM_SCREENS_TALL - 1)
+#VIRTUAL_WIDTH = PIXEL_WIDTH * NUM_SCREENS_WIDE
+#VIRTUAL_HEIGHT = PIXEL_HEIGHT * NUM_SCREENS_TALL
 
-VIRTUAL_WIDTH = PIXEL_WIDTH * NUM_SCREENS_WIDE
-VIRTUAL_HEIGHT = PIXEL_HEIGHT * NUM_SCREENS_TALL
+_size = 4096
+
+TOTAL_WIDTH = _size
+TOTAL_HEIGHT = _size
+VIRTUAL_WIDTH = _size + PIXEL_WIDTH
+VIRTUAL_HEIGHT = _size + PIXEL_HEIGHT
+
 
 BLACK = pygame.Color(0, 0, 0, 255)
 
@@ -34,6 +49,37 @@ def scale_load(path):
     w = s.get_width() * SCALE
     h = s.get_height() * SCALE
     return pygame.transform.scale(s, (w, h))
+
+class Rock(object):
+    def __init__(self, x, y, path):
+        self.img = scale_load(path)
+        self.x = x
+        self.y = y
+        self.w = 32
+        self.h = 32
+    def get_img(self):
+        return self.img
+
+class GlowingRock(object):
+    def __init__(self, x, y, paths):
+        self.frames = []
+        for path in paths:
+            self.frames.append(scale_load(path))
+        self.i = 0
+        self.ticks = 0
+        self.x = x
+        self.y = y
+        self.w = 32
+        self.h = 32
+        self.n = len(self.frames)
+
+    def get_img(self):
+        if self.ticks == 4:
+            self.ticks = 0
+            self.i = (self.i + 1) % self.n
+        else:
+            self.ticks += 1
+        return self.frames[self.i]
 
 class Rover(object):
     groups = [['aa', 'bb', 'cc', 'dd'], ['bd', 'ca', 'db', 'ac']]
@@ -85,35 +131,127 @@ class Map(object):
     weighted = [light, light, light, light, medium, medium, dark]
 
     def __init__(self):
-        self.img = pygame.Surface((VIRTUAL_WIDTH, VIRTUAL_HEIGHT))
+        if False and os.path.exists('save/terrain.png'):
+            self.img = pygame.image.load('save/terrain.png')
+        else:
+            self.img = pygame.Surface((VIRTUAL_WIDTH, VIRTUAL_HEIGHT))
+    
+            self.img.fill(self.light)
 
-        self.img.fill(self.light)
-        for h in range(0, NUM_SCREENS_TALL):
-            for w in range(0, NUM_SCREENS_WIDE):
-                ## debugging colors
-                #r = (h + 1) * 51
-                #g = (w + 1) * 51
-                #b = ((h + w) % 2) * 100
-                #color = pygame.Color(r, g, b, 255)
-                color = self.light
+            #for h in range(0, NUM_SCREENS_TALL):
+            #    for w in range(0, NUM_SCREENS_WIDE):
+            #        ## debugging colors
+            #        #r = (h + 1) * 51
+            #        #g = (w + 1) * 51
+            #        #b = ((h + w) % 2) * 100
+            #        #color = pygame.Color(r, g, b, 255)
+            #        color = self.light
+            #        rect = pygame.Rect(w * PIXEL_WIDTH, h * PIXEL_HEIGHT, PIXEL_WIDTH, PIXEL_HEIGHT)
+            #        self.img.fill(color, rect)
+            #        #self.snowscreen(rect)
 
-                rect = pygame.Rect(w * PIXEL_WIDTH, h * PIXEL_HEIGHT, PIXEL_WIDTH, PIXEL_HEIGHT)
+            self.generate_altitude()
 
-                self.img.fill(color, rect)
-                #self.snowscreen(rect)
+            pygame.image.save(self.img, 'save/terrain.png')
 
         self.wheelmarks = self.init_wheelmarks()
         self.rotate_wheelmarks(0)
 
-    def snowscreen(self, rect):
-        print 'snow', rect
-        for y in range(rect.top, rect.top + rect.h, 4):
-            for x in range(rect.left, rect.left + rect.w, 4):
+    def snowscreen(self):
+        for y in range(0, TOTAL_HEIGHT, 4):
+            for x in range(0, TOTAL_WIDTH, 4):
                 color = random.choice(self.weighted)
                 pygame.gfxdraw.box(self.img, pygame.Rect(x, y, 4, 4), color)
+        self.sync_doubled_area()
+
+    def sync_doubled_area(self):
+        area = pygame.Rect(0, 0, PIXEL_WIDTH, VIRTUAL_HEIGHT)
+        dest = pygame.Rect(TOTAL_WIDTH, 0, PIXEL_WIDTH, VIRTUAL_HEIGHT)
+        self.img.blit(self.img, dest, area)
+
+        area = pygame.Rect(0, 0, VIRTUAL_WIDTH, PIXEL_HEIGHT)
+        dest = pygame.Rect(0, TOTAL_HEIGHT, VIRTUAL_WIDTH, PIXEL_HEIGHT)
+        self.img.blit(self.img, dest, area)
 
     def generate_altitude(self):
-        pass
+        self.snowscreen()
+        return
+
+        size = 2
+        while size < TOTAL_WIDTH / 2:
+            size *= 2
+
+        print 'generating altitude, size=%r...' % size
+
+        # allocate 4097x4097 numbers (16 million of them!)
+        data = []
+        for y in range(0, (size + 1)):
+            data.append([0] * (size + 1))
+
+        x1 = y1 = 0
+        x2 = y2 = size
+
+        for y in (y1, y2):
+            for x in (x1, x2):
+                data[y][x] = random.randint(0, 100)
+        self.diasquare(data, x1, y1, x2, y2, -10000, 10000)
+
+        zmin = min([min(row) for row in data])
+        zmax = max([max(row) for row in data])
+        zrange = zmax - zmin
+
+        def scale(z, nmin, nmax):
+            c = ((z - zmin) * 150) / zrange + random.randint(-50, 50)
+            return min(nmax, max(nmin, c))
+
+        for y in range(0, TOTAL_HEIGHT / 2):
+            ay = y * 2
+            for x in range(0, TOTAL_WIDTH / 2):
+                ax = x * 2
+
+                r = scale(100 + data[y][x], 100, 255)
+                g = scale(30 + data[y][x], 30, 120)
+                b = scale(data[y][x], 10, 70)
+
+                color = pygame.Color(r, g, b, 255)
+                pygame.gfxdraw.box(self.img, pygame.Rect(ax, ay, 2, 2), color)
+
+        # copy to "redundant" area
+        self.sync_doubled_area()
+        #area = pygame.Rect(0, 0, PIXEL_WIDTH, VIRTUAL_HEIGHT)
+        #dest = pygame.Rect(TOTAL_WIDTH, 0, PIXEL_WIDTH, VIRTUAL_HEIGHT)
+        #self.img.blit(self.img, dest, area)
+        #
+        #area = pygame.Rect(0, 0, TOTAL_WIDTH, PIXEL_HEIGHT)
+        #dest = pygame.Rect(0, TOTAL_HEIGHT, TOTAL_WIDTH, PIXEL_HEIGHT)
+        #self.img.blit(self.img, dest, area)
+        print 'done'
+
+    def avg(self, xs, rmin, rmax):
+        return sum(xs) / len(xs) + random.randint(rmin, rmax) + random.randint(-10, 10)
+
+    def diasquare(self, data, x1, y1, x2, y2, rmin, rmax):
+        dx, dy = x2 - x1, y2 - y1
+        assert dx == dy
+
+        # get the average for the middle of the square
+        my, mx = y1 + dy / 2, x1 + dx / 2
+        pts = [data[y1][x1], data[y1][x2], data[y2][x1], data[y2][x2]]
+        data[my][mx] = self.avg(pts, rmin, rmax)
+
+        # using the square find the diamond
+        for y in (y1, y2):
+            data[y][mx] = self.avg([data[y][x1], data[y][x2], data[my][mx]], rmin, rmax)
+        for x in (x1, x2):
+            data[my][x] = self.avg([data[y1][x], data[y2][x], data[my][mx]], rmin, rmax)
+
+        if dx >= 4:
+            rmin /= 2
+            rmax /= 2
+            self.diasquare(data, x1, y1, mx, my, rmin, rmax)
+            self.diasquare(data, mx, y1, x2, my, rmin, rmax)
+            self.diasquare(data, x1, my, mx, y2, rmin, rmax)
+            self.diasquare(data, mx, my, x2, y2, rmin, rmax)
 
     def rotate_wheelmark(self, wm, angle):
         return pygame.transform.rotate(wm, angle)
@@ -173,6 +311,9 @@ class Game(object):
         self.ox = 0
         self.oy = 0
 
+        # rocks
+        self.rocks = [Rock(200, 200, 'gfx/rock1.png')]
+
         # rover stuff
         self.rover = Rover()
         self.rx = 500
@@ -200,7 +341,7 @@ class Game(object):
     def shift_map_left(self):
         nx = self.rx - 200
         if nx < 0:
-            #print 'big shift left (ny=%r oy=%r ry=%r)' % (ny, self.oy, self.ry)
+            print 'big shift left (nx=%r ox=%r rx=%r)' % (nx, self.ox, self.rx)
             self.ox = nx + TOTAL_WIDTH
             self.rx += TOTAL_WIDTH
         else:
@@ -211,7 +352,7 @@ class Game(object):
     def shift_map_right(self):
         nx = self.rx - 800
         if nx > TOTAL_WIDTH:
-            #print 'big shift right (ny=%r oy=%r ry=%r)' % (ny, self.oy, self.ry)
+            print 'big shift right (nx=%r ox=%r rx=%r)' % (nx, self.ox, self.rx)
             self.ox = nx - TOTAL_WIDTH
             self.rx -= TOTAL_WIDTH
         else:
@@ -222,7 +363,7 @@ class Game(object):
     def shift_map_up(self):
         ny = self.ry - 200
         if ny < 0:
-            #print 'big shift up (ny=%r oy=%r ry=%r)' % (ny, self.oy, self.ry)
+            print 'big shift up (ny=%r oy=%r ry=%r)' % (ny, self.oy, self.ry)
             self.oy = ny + TOTAL_HEIGHT
             self.ry += TOTAL_HEIGHT
         else:
@@ -234,7 +375,7 @@ class Game(object):
     def shift_map_down(self):
         ny = self.ry - 500
         if ny > TOTAL_HEIGHT:
-            #print 'big shift down (ny=%r oy=%r ry=%r)' % (ny, self.oy, self.ry)
+            print 'big shift down (ny=%r oy=%r ry=%r)' % (ny, self.oy, self.ry)
             self.oy = ny - TOTAL_HEIGHT
             self.ry -= TOTAL_HEIGHT
         else:
@@ -286,6 +427,23 @@ class Game(object):
         dest = self.get_map_screen_rect()
         self.screen.blit(self.map.img, dest, area)
 
+    def get_rock_screen_rect(self, rock):
+        w2, h2 = rock.w / 2, rock.h / 2
+        left, top = rock.x, rock.y
+        if left < PIXEL_WIDTH and self.ox > PIXEL_WIDTH:
+            left += TOTAL_WIDTH
+        if top < PIXEL_HEIGHT and self.oy > PIXEL_HEIGHT:
+            top += TOTAL_HEIGHT
+        rect = pygame.Rect(left - self.ox - w2, top - self.oy - h2, rock.w, rock.h)
+        #print left, top, self.ox, self.oy, TOTAL_WIDTH, TOTAL_HEIGHT, rect
+        return rect
+
+    def draw_rocks(self):
+        for rock in self.rocks:
+            dest = self.get_rock_screen_rect(rock)
+            img = rock.get_img()
+            self.screen.blit(img, dest)
+
     def get_rover_screen_rect(self):
         w = self.curr_rover_img.get_width()
         h = self.curr_rover_img.get_height()
@@ -304,6 +462,7 @@ class Game(object):
 
     def draw(self):
         self.draw_map()
+        self.draw_rocks()
         self.draw_rover()
 
     def run(self):
