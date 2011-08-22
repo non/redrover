3d60cfd114c5046638ce0fabc467e972029fba22
Erik Osheim <d_m@plastic-idolatry.com>
2011-08-21 21:33:59 -0400

    endings basically work ok

--- src/game.py
+++ src/game.py
@@ -28,19 +28,19 @@ WHITE = pygame.Color(255, 255, 255, 255)
 BLACK = pygame.Color(0, 0, 0, 255)
 
 GOAL1_X = PIXEL_WIDTH + 234
-GOAL1_Y = PIXEL_HEIGHT * 2 + 452
+GOAL1_Y = PIXEL_HEIGHT + 452
 
-GOAL2A_X = PIXEL_WIDTH * 2 + 400
-GOAL2A_Y = 500
+GOAL2_X = PIXEL_WIDTH * 2 + 480
+GOAL2_Y = 580
 
-GOAL2B_X = PIXEL_WIDTH * 2 + 560
-GOAL2B_Y = 660
-
-GOAL2C_X = PIXEL_WIDTH * 2 + 400
-GOAL2C_Y = 500
-
-GOAL2D_X = PIXEL_WIDTH * 2 + 560
-GOAL2D_Y = 660
+#GOAL2A_X = PIXEL_WIDTH * 2 + 400
+#GOAL2A_Y = 500
+#GOAL2B_X = PIXEL_WIDTH * 2 + 560
+#GOAL2B_Y = 660
+#GOAL2C_X = PIXEL_WIDTH * 2 + 400
+#GOAL2C_Y = 500
+#GOAL2D_X = PIXEL_WIDTH * 2 + 560
+#GOAL2D_Y = 660
 
 # deg:     0->N   90->E    180->S  270->W
 # ccw:     0->N  270->E    180->S   90->W
@@ -55,7 +55,7 @@ def get_dxdy(deg, dist):
     return math.cos(theta) * dist, -math.sin(theta) * dist
 
 def scale_load(path):
-    s = pygame.image.load(path)
+    s = pygame.image.load(path).convert_alpha()
     w = s.get_width() * SCALE
     h = s.get_height() * SCALE
     return pygame.transform.scale(s, (w, h))
@@ -75,6 +75,7 @@ class Rock(object):
         self.pts = pts
         self.seen = False
         self.panel = False
+        self.gem = False
     def get_img(self):
         return self.img
 
@@ -129,7 +130,8 @@ class Map(object):
 
     def __init__(self):
         if os.path.exists('save/terrain.png'):
-            self.img = pygame.image.load('save/terrain.png')
+            self.img = pygame.image.load('save/terrain.png').convert()
+            self.sync_doubled_area()
         else:
             self.img = pygame.Surface((VIRTUAL_WIDTH, VIRTUAL_HEIGHT))
             self.img.fill(self.light)
@@ -139,7 +141,7 @@ class Map(object):
 
         panel = scale_load('gfx/panel.png')
 
-        self.panelrect = pygame.Rect(PIXEL_WIDTH + 234, PIXEL_HEIGHT + 452, 32, 32)
+        self.panelrect = pygame.Rect(GOAL1_X, GOAL1_Y, 32, 32)
         self.img.blit(panel, self.panelrect)
 
         self.wheelmarks = self.init_wheelmarks()
@@ -218,14 +220,40 @@ class Game(object):
         if color is None: color = WHITE
         return self.bigfont.render(s, False, color)
 
+    def dirtify(self, x, y):
+        color = random.choice(self.map.kinds)
+        self.map.img.set_at((x, y), color)
+
     def init_rocks(self):
         nrocks = 40
 
         imgs = [scale_load('gfx/rock%d.png' % i) for i in range(1, 7)]
         pts = [10, 10, 10, 10, 25, 25, 25, 50, 50, 100, 200]
+        gem = scale_load('gfx/panel.png')
         self.rocks = [
-            Rock.frompath(PIXEL_WIDTH + 234 + 16, PIXEL_HEIGHT + 452 + 16, 'gfx/cover.png', 1000)
+            #Rock.frompath(PIXEL_WIDTH + 234 + 16, PIXEL_HEIGHT + 452 + 16, 'gfx/cover.png', 1000)
+            Rock.frompath(GOAL1_X + 16, GOAL1_Y + 16, 'gfx/cover.png', 1000),
+            Rock.frompath(GOAL2_X + 16 + 500, GOAL2_Y + 16 - 350, 'gfx/gem.png', 2000),
+
+            Rock(GOAL2_X + 16 + 100, GOAL2_Y + 16, imgs[0], 100),
+            Rock(GOAL2_X + 16 - 100, GOAL2_Y + 16, imgs[2], 100),
+            Rock(GOAL2_X + 16, GOAL2_Y + 16 + 100, imgs[3], 100),
+            Rock(GOAL2_X + 16, GOAL2_Y + 16 - 100, imgs[5], 100),
+
+            Rock(GOAL2_X + 16 + 70, GOAL2_Y + 16 + 70, imgs[1], 100),
+            Rock(GOAL2_X + 16 - 70, GOAL2_Y + 16 + 70, imgs[0], 100),
+            Rock(GOAL2_X + 16 + 70, GOAL2_Y + 16 - 70, imgs[4], 100),
+            Rock(GOAL2_X + 16 - 70, GOAL2_Y + 16 - 70, imgs[2], 100),
         ]
+        self.rocks[0].panel = True
+        self.rocks[1].gem = True
+
+        for i in range(-16, 17):
+            for j in range(-16, 17):
+                k2 = math.sqrt(i * i + j * j)
+                if k2 > 18:
+                    continue
+                self.dirtify(GOAL2_X + 16 + i, GOAL2_Y + 16 + j)
 
         for i in range(0, nrocks):
             while True:
@@ -242,10 +270,12 @@ class Game(object):
 
         self.maxscore = min(9999, sum([rock.pts for rock in self.rocks]))
 
-    def __init__(self):
+    def __init__(self, screen, fullscreen):
         self.done = False
         self.screen = None
         self.clock = None
+        self.screen = screen
+        self.fullscreen = fullscreen
 
         # text stuff
         self.banner = True
@@ -290,6 +320,65 @@ class Game(object):
         self.vdelta = 0
         self.adelta = 0
 
+        # messages
+        self.timed_msgs = [
+            [5000, "TIME TO GO TO WORK", 0],
+            [15000, "THIS RESEARCH WON'T DO ITSELF", 0],
+            [45000, "STILL HERE", 0],
+            [75000, "SO! BORED!", 0],
+        ]
+
+        self.random_msgs = [
+            ["SURE IS LONELY OUT HERE", -10000],
+            ["MISSION CONTROL? YOU THERE?", -10000],
+            ["FOR SCIENCE!", -10000],
+            ["I MISS THE FACTORY", -10000],
+            ["NOT A VERY BIG PLANET", -10000],
+            ["ROCK... ROCK... STONE!", -10000],
+            ["DIDN'T THEY SEND OTHERS?", -10000],
+            ["HMMMMM...", -10000],
+            ["BEEP BOOP BEEP", -10000],
+            ["EXTERMINATE! EXTERMINATE!", -10000],
+            ["ALL WORK AND NO PLAY...", -10000],
+            ["WORK, WORK, WORK", -10000],
+            ["SIGH...", -10000],
+        ]
+
+        self.zones = [
+            [pygame.Rect(GOAL1_X - 200, GOAL1_Y - 200, 432, 432), 'I HAVE A STRANGE FEELING', -10, False],
+            [pygame.Rect(GOAL2_X - 200, GOAL2_Y - 200, 560, 560), 'AM I BEING WATCHED?', -10, False],
+        ]
+
+        self.next_msg_delta = random.randint(15000, 20000)
+
+        # winning
+        self.button_pushed = 0
+        self.door_opened = False
+        self.door_rect = pygame.Rect(GOAL1_X + 300, GOAL1_Y, 160, 160)
+        self.door_win_rect = pygame.Rect(GOAL1_X + 360, GOAL1_Y + 60, 40, 40)
+        self.door = scale_load('gfx/door.png')
+        self.door_ending = False
+
+        self.door_text0 = self.mktext("YOU FOUND MORE THAN JUST ROCKS!")
+        self.door_text1 = self.mktext("WHO KNOWS WHAT AWAITS YOU")
+        self.door_text2 = self.mktext("IN THE UNDERGROUND LAIR?")
+        self.door_text3 = self.mktext("BUT YOU WILL DISCOVER IT...")
+        self.door_text4 = self.mktext("FOR SCIENCE!")
+        self.door_text5 = self.mkbigtext("THE END")
+
+        self.signal_placed = 0
+        self.signalrect = pygame.Rect(GOAL2_X, GOAL2_Y, 32, 32)
+        self.ship_summoned = False
+        self.ship_ending = False
+
+        self.ship_text0 = self.mktext("YOU LEAVE IN A BEAM LIGHT!", BLACK)
+        self.ship_text1 = self.mktext("WHO KNOWS WHAT AWAITS YOU", BLACK)
+        self.ship_text2 = self.mktext("ABOARD THIS ALIEN CRAFT?", BLACK)
+        self.ship_text3 = self.mktext("BUT YOU WILL DISCOVER IT...", BLACK)
+        self.ship_text4 = self.mktext("FOR SCIENCE!", BLACK)
+        self.ship_text5 = self.mkbigtext("THE END", BLACK)
+
+
     def addpoints(self, pts):
         self.score = min(9999, self.score + pts)
         self.scoretxt = self.mktext("SCORE %4d" % self.score)
@@ -309,7 +398,7 @@ class Game(object):
     def shift_map_left(self):
         nx = self.rx - 200
         if nx < 0:
-            print 'big shift left (nx=%r ox=%r rx=%r)' % (nx, self.ox, self.rx)
+            #print 'big shift left (nx=%r ox=%r rx=%r)' % (nx, self.ox, self.rx)
             self.ox = nx + TOTAL_WIDTH
             self.rx += TOTAL_WIDTH
         else:
@@ -320,7 +409,7 @@ class Game(object):
     def shift_map_right(self):
         nx = self.rx - 800
         if nx > TOTAL_WIDTH:
-            print 'big shift right (nx=%r ox=%r rx=%r)' % (nx, self.ox, self.rx)
+            #print 'big shift right (nx=%r ox=%r rx=%r)' % (nx, self.ox, self.rx)
             self.ox = nx - TOTAL_WIDTH
             self.rx -= TOTAL_WIDTH
         else:
@@ -331,7 +420,7 @@ class Game(object):
     def shift_map_up(self):
         ny = self.ry - 200
         if ny < 0:
-            print 'big shift up (ny=%r oy=%r ry=%r)' % (ny, self.oy, self.ry)
+            #print 'big shift up (ny=%r oy=%r ry=%r)' % (ny, self.oy, self.ry)
             self.oy = ny + TOTAL_HEIGHT
             self.ry += TOTAL_HEIGHT
         else:
@@ -343,7 +432,7 @@ class Game(object):
     def shift_map_down(self):
         ny = self.ry - 500
         if ny > TOTAL_HEIGHT:
-            print 'big shift down (ny=%r oy=%r ry=%r)' % (ny, self.oy, self.ry)
+            #print 'big shift down (ny=%r oy=%r ry=%r)' % (ny, self.oy, self.ry)
             self.oy = ny - TOTAL_HEIGHT
             self.ry -= TOTAL_HEIGHT
         else:
@@ -361,8 +450,9 @@ class Game(object):
         if self.adelta:
             self.map.rotate_wheelmarks(-self.ra)
 
+        rect = self.get_rover_rect()
         if self.vdelta or self.adelta:
-            self.map.track_wheelmarks(self.get_rover_rect())
+            self.map.track_wheelmarks(rect)
 
         if self.rx < self.ox + 200:
             self.shift_map_left()
@@ -373,6 +463,10 @@ class Game(object):
             self.shift_map_up()
         elif self.ry > self.oy + 500:
             self.shift_map_down()
+
+        if self.door_opened and self.door_win_rect.colliderect(rect):
+            print 'DOOR: YOU WIN!!!!!'
+            self.door_ending = True
 
     def move_rover(self):
         if self.vdelta == 0: return
@@ -456,7 +550,10 @@ class Game(object):
     def pick_up_rock(self, rock):
         if not rock.seen:
             rock.seen = True
-            self.setstatus("ANALYZING ROCK! %+d POINTS!" % rock.pts, pygame.Color(0, 255, 0))
+            if rock.panel:
+                self.setstatus("ANALYZING PANEL! %+d POINTS!" % rock.pts, pygame.Color(0, 255, 0))
+            else:
+                self.setstatus("ANALYZING ROCK! %+d POINTS!" % rock.pts, pygame.Color(0, 255, 0))
             self.addpoints(rock.pts)
 
         self.curr_rock = rock
@@ -473,7 +570,10 @@ class Game(object):
 
         self.map.sync_doubled_area()
         self.rocks.remove(rock)
-        self.clawtxt = self.mktext("HOLDING ROCK")
+        if self.curr_rock.panel:
+            self.clawtxt = self.mktext("GOT PANEL")
+        else:
+            self.clawtxt = self.mktext("HOLDING ROCK")
 
     def drop_rock(self):
         self.clawtxt = self.mktext("CLAW EMPTY")
@@ -485,6 +585,12 @@ class Game(object):
         if not self.curr_rock.panel and self.map.panelrect.colliderect(rect):
             self.setstatus("PUSHED THE BUTTON", pygame.Color(0, 255, 0))
             self.setmsg("WHAT WAS THAT SOUND!?")
+            self.button_pushed = 1
+        elif self.curr_rock.gem and self.signalrect.colliderect(rect):
+            self.setstatus("DROPPED THE GEM", pygame.Color(0, 255, 0))
+            self.setmsg("WHAT IS THAT LIGHT!?")
+            self.signal_placed = 1
+            self.ship_ending = True
         else:
             self.setstatus("DROPPED THE ROCK", pygame.Color(255, 255, 255))
 
@@ -525,10 +631,37 @@ class Game(object):
                 self.setstatus("I DON'T SEE A ROCK", pygame.Color(255, 255, 0))
 
     def draw(self):
+        self.draw_other()
         area = self.draw_map()
         self.draw_rocks(area)
         self.draw_rover()
         self.draw_text()
+
+    def draw_other(self):
+        if self.button_pushed and not self.door_opened:
+            i = (((self.button_pushed % 6) / 3) * 6) - 3
+
+            self.ox = self.rx - 350 + i
+            self.oy = self.ry - 350
+
+            if self.button_pushed > 100:
+                self.door_opened = True
+                self.map.img.blit(self.door, self.door_rect)
+            else:
+                self.button_pushed += 1
+    
+                if self.button_pushed % 30 >= 15:
+                    color = self.map.medium
+                else:
+                    color = self.map.dark
+         
+                pygame.gfxdraw.box(self.map.img, self.door_rect, color)
+
+        if self.signal_placed and not self.ship_summoned:
+            print self.signal_placed
+            if self.signal_placed > 100:
+                self.ship_summoned = True
+            self.signal_placed += 1
 
     def draw_banner(self):
         self.screen.blit(self.bannertxt, (80, 80))
@@ -546,13 +679,53 @@ class Game(object):
         self.statustxt = self.mktext(s, color)
         self.statust = pygame.time.get_ticks()
 
-    def run(self):
+    def check_ambient_msg(self):
+        if self.msgtxt is not None: return
 
+        t = pygame.time.get_ticks()
+
+        rect = self.get_rover_rect()
+        for zone in self.zones:
+            zrect, zmsg, zt, zstat = zone
+            if rect.colliderect(zrect):
+                if zstat:
+                    pass
+                elif t - zt > 10.0:
+                    zone[2] = t
+                    self.setmsg(zmsg)
+                    zone[3] = True
+                    return
+            else:
+                zone[3] = False
+
+        if self.timed_msgs and self.timed_msgs[0][0] < t:
+            self.setmsg(self.timed_msgs[0][1])
+            del self.timed_msgs[0]
+            return
+
+        if t - self.msgt > self.next_msg_delta:
+            self.next_msg_delta = random.randint(15000, 20000)
+            while True:
+                msgrec = random.choice(self.random_msgs)
+                msg, last = msgrec
+                if t - last > 20000:
+                    self.setmsg(msg)
+                    msgrec[1] = t
+                    return
+
+    def open_door(self):
+        if self.button_pushed >= 10:
+            self.door_opened = True
+        
+        self.button_pushed += 1
+
+    def summon_ship(self):
+        pass
+
+    def run(self):
         pygame.mixer.music.load('snd/redrover.mp3')
         pygame.mixer.music.play(-1) # FIXME
 
-        flags = 0
-        self.screen = pygame.display.set_mode((PIXEL_WIDTH, PIXEL_HEIGHT), flags)
         self.clock = pygame.time.Clock()
 
         self.draw()
@@ -566,31 +739,81 @@ class Game(object):
                     self.banner = False
                     break
 
-        msgs = [
-            (5000, "TIME TO GO TO WORK"),
-            (15000, "THIS RESEARCH WON'T DO ITSELF"),
-            (35000, "SURE IS LONELY OUT HERE"),
-            (50000, "MISSION CONTROL? YOU THERE?"),
-            (80000, "I HAVE A STRANGE FEELING"),
-        ]
-
         self.setstatus("DEPLOY CLAW WHEN OVER ROCKS")
 
+        alpha = 255
         while not self.done:
             self.clock.tick(60)
 
-            if self.msgtxt is None and msgs and msgs[0][0] < pygame.time.get_ticks():
-                self.setmsg(msgs[0][1])
-                del msgs[0]
-
             for event in pygame.event.get():
                 self.handle(event)
-            self.handle_rover()
 
-            self.draw()
+            if self.door_ending:
+                pygame.gfxdraw.box(self.screen, self.get_map_screen_rect(), BLACK)
+                if alpha > 0:
+                    alpha -= 2
+                    self.map.img.set_alpha(alpha)
+                    self.draw_map()
+                else:
+                    self.screen.blit(self.door_text0, (10, 90))
+                    self.screen.blit(self.door_text1, (30, 190))
+                    self.screen.blit(self.door_text2, (30, 240))
+                    self.screen.blit(self.door_text3, (20, 320))
+                    self.screen.blit(self.door_text4, (20, 370))
+                    self.screen.blit(self.door_text5, (150, 490))
+
+            elif self.ship_ending:
+                pygame.gfxdraw.box(self.screen, self.get_map_screen_rect(), WHITE)
+                if alpha > 0:
+                    alpha -= 2
+                    self.map.img.set_alpha(alpha)
+                    self.draw_map()
+                    self.curr_rover_img.set_alpha(alpha)
+                    self.draw_rover()
+                else:
+                    self.screen.blit(self.ship_text0, (10, 90))
+                    self.screen.blit(self.ship_text1, (30, 190))
+                    self.screen.blit(self.ship_text2, (30, 240))
+                    self.screen.blit(self.ship_text3, (20, 320))
+                    self.screen.blit(self.ship_text4, (20, 370))
+                    self.screen.blit(self.ship_text5, (150, 490))
+
+            else:
+                self.check_ambient_msg()
+    
+                if self.button_pushed and not self.door_opened:
+                    pass
+                elif self.signal_placed and not self.ship_summoned:
+                    pass
+                else:
+                    self.handle_rover()
+
+                self.draw()
             pygame.display.flip()
 
         pygame.mixer.fadeout(300)
+
+    def show_door_ending(self):
+        full = pygame.Rect(0, 0, PIXEL_WIDTH, PIXEL_HEIGHT)
+        while alpha > 0:
+            self.clock.tick(60)
+            pygame.gfxdraw.box(self.screen, full, BLACK)
+            self.map.img.set_alpha(alpha)
+            alpha -= 1
+            self.draw_map()
+            pygame.display.flip()
+
+        while True:
+            pygame.gfxdraw.box(self.screen, full, BLACK)
+            pygame.display.flip()
+
+    def toggle_fullscreen(self):
+        flags = pygame.HWSURFACE | pygame.DOUBLEBUF
+        if self.fullscreen:
+            self.screen = pygame.display.set_mode((PIXEL_WIDTH, PIXEL_HEIGHT), flags)
+        else:
+            self.screen = pygame.display.set_mode((PIXEL_WIDTH, PIXEL_HEIGHT), flags | pygame.FULLSCREEN)
+        self.fullscreen = not self.fullscreen
 
     def handle(self, ev):
         MOVE = 2
@@ -611,7 +834,10 @@ class Game(object):
             elif ev.key == pygame.K_RIGHT:
                 self.start_turn(ANGLE)
 
-            elif ev.key == pygame.K_LCTRL:
+            elif ev.key == pygame.K_TAB:
+                self.toggle_fullscreen()
+
+            elif ev.key == pygame.K_LCTRL or ev.key == pygame.K_RCTRL:
                 if self.curr_rock:
                     self.crush_rock()
                 else:
@@ -645,5 +871,9 @@ if __name__ == "__main__":
     pygame.font.init()
     pygame.mixer.init(frequency=44100)
 
-    g = Game()
+    #flags = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.FULLSCREEN
+    flags = pygame.HWSURFACE | pygame.DOUBLEBUF
+    screen = pygame.display.set_mode((PIXEL_WIDTH, PIXEL_HEIGHT), flags)
+
+    g = Game(screen, False)
     g.run()
